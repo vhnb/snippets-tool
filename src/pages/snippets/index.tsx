c3a1ec9f3b3c1dc7bc15b56675087da2904a0532
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2025-01-25 05:08:25 -0300

    Update index.tsx

--- src/pages/snippets/index.tsx
+++ src/pages/snippets/index.tsx
@@ -66,7 +66,7 @@ export default function Snippets({ user }: HomeProps) {
             })
         }
         fetchSnippets()
-    }, [user?.email])
+    }, [user?.email, searchTerm])
 
     return (
         <>
