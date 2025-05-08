0fdde67a11203bf9c517e7e2dae840128395c3c6
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2025-05-08 15:53:56 -0300

    Update firebaseConnection.ts

--- src/service/firebaseConnection.ts
+++ src/service/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY_FIREBASE,
-  authDomain: process.env.AUTHDOMAIN_FIREBASE,
-  projectId: process.env.PROJECTID_FIREBASE,
-  storageBucket: process.env.STORAGEBUCKET_FIREBASE,
-  messagingSenderId: process.env.MESSAGEINGSENDERID_FIREBASE,
-  appId: process.env.APPID_FIREBASE
+  apiKey: process.env.NEXT_PUBLIC_APIKEY_FIREBASE,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN_FIREBASE,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID_FIREBASE,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET_FIREBASE,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGEINGSENDERID_FIREBASE,
+  appId: process.env.NEXT_PUBLIC_APPID_FIREBASE
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
